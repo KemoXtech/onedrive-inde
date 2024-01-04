3eba9416dd5d04afc16bf1644fdc91e9cfd83934
KemoXtech <93053669+KemoXtech@users.noreply.github.com>
2024-01-04 04:53:15 +0200

    Update FourOhFour.tsx

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -26,11 +26,11 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
             and open devtools for more details, or seek help at{' '}
             <a
               className="text-blue-600 hover:text-blue-700 hover:underline"
-              href="https://github.com/iRedScarf/onedrive-index/discussions"
+              href="t.me/devproz"
               target="_blank"
               rel="noopener noreferrer"
             >
-              onedrive-index discussions
+       
             </a>
             .
           </Trans>
